64bf09642764d1d87e7dec7dd388ad30967ce983
yeezyseason1 <115050157+yeezyseason1@users.noreply.github.com>
2022-10-04 19:12:04 -0500

    Create animals.cpp

---
+++ animals.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "animals.h"
+
+using namespace std;
+
+int main () {
+    Age monkey;
+    monkey.getAge();
+
+    AnimalFood bear;
+    bear.getFood();
+
+    VetVisit lion;
+    lion.getVetVisit();
+}
