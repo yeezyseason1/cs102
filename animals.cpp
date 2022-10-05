2f135342b7d9855b21dcbe18fac6e5100007adee
yeezyseason1 <115050157+yeezyseason1@users.noreply.github.com>
2022-10-04 19:37:22 -0500

    Update animals.cpp

--- animals.cpp
+++ animals.cpp
@@ -1,3 +1,6 @@
+//all of the actual work has been done in the .h file. here all i do is declare my objects and use 
+//them for the output portion of the serived class they are a part of.
+
 #include <iostream>
 #include <string>
 #include "animals.h"
