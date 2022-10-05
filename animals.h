d9bbb47e2c7013631a01cabe6563f8e2f3d8c4c6
yeezyseason1 <115050157+yeezyseason1@users.noreply.github.com>
2022-10-04 19:35:42 -0500

    Update animals.h

--- animals.h
+++ animals.h
@@ -1,8 +1,12 @@
+//this is for the inherited classes challenge problem, i used three derived classes (Age, AnimalFood, and VetVisit) from the base class (Animals)
+//using general info collected from the base class constructor and stored in the base class, the derived classes calculate the desired outputs.
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+//declaring all the variable
 class Animals
 {
 protected:
@@ -17,6 +21,7 @@ int weight;
 
 public:
 
+//constructor which is getting the general info on every animal
 Animals(){
     cout << "Enter Name: ";
     cin >> name;
@@ -29,6 +34,7 @@ Animals(){
 }
 }; 
 
+//class made to determine animals age using their general info and todays date
 class Age: public Animals
 {
 private:
@@ -36,6 +42,7 @@ int age;
 
 public:
 
+//determines age
 Age() {
     if (birthmonth <= currentmonth && birthday <= currentday) {
         age = currentyear - birthyear;
@@ -45,11 +52,13 @@ Age() {
     }
 }
 
+//outputs age
 void getAge(){
     cout << name <<  " is " << age << " years old." << endl;
 }
 };
 
+//determines how much food animals eat in a day based on their weight
 class AnimalFood: public Animals
 {
 private:
@@ -57,15 +66,20 @@ float fooddaily;
 
 public:
 
+//calculates their food needs based on the generalization i found on google that animals
+//tend to eat 2.25% of their body weight.
 AnimalFood() {
     fooddaily = weight * 0.025;
 }
 
+//outputs their approximate food intake
 void getFood() {
     cout << name << " eats approximately " << fooddaily << " pounds of food daily" << endl;
 }
 };
 
+//determines how many days until the animals next vet visit based on how long it has been 
+//since the last one. i decided animals at this zoo should see a vet every 4 weeks.
 class VetVisit: public Animals
 {
 private:
@@ -73,12 +87,15 @@ int days_since_vet;
 int days_until_vet;
 
 public:
+
+//asks for how many days it has been since last vet visit and determines how many days until next one
 VetVisit() {
     cout << "Enter how many days it has been since " << name << " has been to the vet: " << endl;
     cin >> days_since_vet;
     days_until_vet = 28 - days_since_vet; 
 }
 
+//outputs how many days are left until next visit
 void getVetVisit() {
     if (days_until_vet > 0) 
         cout << name << " will need to be seen by a veterinarian in " << days_until_vet << " days." << endl;
