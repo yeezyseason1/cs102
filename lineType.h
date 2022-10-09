d6565253e4e33164aebf442b2495fcf1d35768dc
yeezyseason1 <115050157+yeezyseason1@users.noreply.github.com>
2022-10-09 13:24:27 -0500

    Update lineType.h

--- lineType.h
+++ lineType.h
@@ -4,113 +4,94 @@
 
 using namespace std;
 
-class LineType 
+class LineType
 {
-private:
-
-    //we only need 3 attributes for storing "a", "b", and "c" in the equation
-    //ax + by = c
-
-    double a, b, c;
-
 public:
-
-    //double slope
-    //overload
 
     LineType()
     {
         a = 0;
         b = 0;
         c = 0;
-        set_line(a, b, c);
     }
 
-    LineType(double a, double b, double c)
+    LineType(double A, double B, double C)
     {
-        set_line(a, b, c);
+        a = A;
+        b = B;
+        c = C;
     }
 
-    //This function needs to return a bool but also return the slope if it exists
-    //to do so, you need to use reference parameters. Remember that reference parameters
-    //are ones that "connect" to the passed in variable. and any change in the 
-    //reference parameter's value will affects whatever passed into the function.
-    //Here we make "input" a reference parameter by inserting an "&" sign in front of the name.
-    //This allows us to pass back the slope via the input.
-
-    bool get_slope(double&input)
-    {
-        if (b != 0)
-        {
-            input = -a / b;
-            return true;
+    bool get_slope(double& slope) {
+        if (b != 0) {
+            slope = -a / b;
+            return true; 
         }
         else
             return false;
     }
 
-    //declare functions
-    //void set_line(double a = 0, double b = 0, double c = 0);
-
-    //void get_slope(doube sl);
-
-    /*
-    string to_string(string sl)
-    {
-        if(xco == 0)
-        {
-            char*intStr = itoa(xco);
-            string str = string(inStr);
-        }
-
-        sl = str;
-
-        else
-            slope = -a / b;
-
-        return sl;
-
-    }
-    */
-
-   //void are_equal(bool ae);
-
-   //FUNCTIONS
-
-   //Function to set the line
-
-   void set_line(double new_a,double new_b,double new_c)
-   {
-       a = new_a;
-       b = new_b;
-       c = new_c;
-   }
-
-   /*
-   void get_slope(double sl)
-   {
-        if (xco == 0)
-            hor = true;
-        else if (yco == 0)
-            ver = true;
+    bool are_equal(LineType line) {
+        if (a == line.a)
+            return true;
+            
         else 
-            sl = -xco / yco;
+            return false;
     }
-    
-    void to_spring(string slope)
-    {
-        if(hor = true)
-            slope = "0.00";
-        else if(ver = true)
-            slope = "undefined";
-        else slope = sl;
-    }
-    */
 
-   /*
-   bool are_equal(bool ae)
-   {
-       if
-   }
-   */
+    bool are_parallel(LineType line) {
+        double slope1 = -a / b;
+        double slope2 = -line.a / line.b;
+        
+        if (slope1 == slope2)
+            return true;
+            
+        else 
+            return false;
+    }
+
+    bool are_perpendicular(LineType line) {
+        double slope1 = -a / b;
+        double slope2 = -line.a / line.b;
+        
+        if (slope1 != slope2)
+            return true;
+            
+        else 
+            return false;
+    }
+
+    bool get_intersection(LineType line, double& x, double& y) {
+        double slope1 = -a / b;
+        double slope2 = -line.a / line.b;
+        
+        if (slope1 == slope2)
+            return false;
+            
+        else {
+            x = ((b * -line.c) - (line.b * -c))/((a * line.b) - (line.a * b));
+            //y = ((-c * line.a) - (-c * line.a))/((a * line.b) - (line.a * b));
+            y = ((b * -line.c) - (line.b * -c))/((a * line.b) - (line.a * b));
+            return true;
+        }
+    }
+
+    void set_line(double a, double b, double c) {
+        
+    }
+
+private: 
+    double a;
+    double b;
+    double c;
+
+    double get_a() {
+
+    }
+    double get_b() {
+
+    }
+    double get_c() {
+
+    }
 };
